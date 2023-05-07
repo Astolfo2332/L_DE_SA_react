ee35eb740666aba35d9acd6ecf448e14c2ad05c5
Miguel <migue19sep@gmail.com>
2023-05-07 17:16:58 -0500

    un poquito después del login

--- ldesa/src/components/header/header.js
+++ ldesa/src/components/header/header.js
@@ -1,19 +1,23 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexto/authContext";
+import "./header.css"
+
 
 function Header({ setUser }) {
 
   const user = useContext(AuthContext)
-
+  const logoOscuro=require("../logos/logoOscuro.PNG")
   function logout() {
     setUser(null);
     localStorage.removeItem('user')
   }
 
   return (
-    <div>
-      {user ? <button className='btn' type="button" onClick={logout}>logout</button> : null}
-    </div>
+
+    <header className="header-form">
+      <img className="element" src={logoOscuro} alt="LdeSA" ></img>
+      {user ? <button className='bt' type="button" onClick={logout}>logout</button> : null}
+    </header>
   )
 }
 
--- ldesa/src/components/login/login.js
+++ ldesa/src/components/login/login.js
@@ -8,11 +8,12 @@ const user = {
 }
 
 function Login({ setUser }) {
-  var IconoOscuro=require("../logos/iconoOscuro.PNG")
-  var iconoClaro=require("../logos/iconoClaro.PNG")
-  var logoOscuro=require("../logos/logoOscuro.PNG")
+  const IconoOscuro=require("../logos/iconoOscuro.PNG")
   const [email, setEmail] = useState()
   const [password, setPasword] = useState()
+
+  
+
 
   function login() {
     if (email === user.email && password === user.password) {
@@ -23,24 +24,18 @@ function Login({ setUser }) {
   }
 
   return (
-
-    
-
     <div className="login-form form">
-      <header> 
-        <img src={logoOscuro} alt="LdeSA"></img>
         <img src={IconoOscuro} alt="LdeSA"></img>
-      </header>
       <div className="login-form box">
       <h1 >Qué es LdeSA?</h1> 
-      <p >Once upon a midnight dreary, while I pondered, weak and weary,
-Over many a quaint and curious volume of forgotten lore—
+      <p>Once upon a midnight dreary, while I pondered, weak and weary,
+    Over many a quaint and curious volume of forgotten lore—
     While I nodded, nearly napping, suddenly there came a tapping,
-As of some one gently rapping, rapping at my chamber door.
+    As of some one gently rapping, rapping at my chamber door.
 “’Tis some visitor,” I muttered, “tapping at my chamber door—
             Only this and nothing more.”
 
-    Ah, distinctly I remember it was in the bleak December;
+Ah, distinctly I remember it was in the bleak December;
 And each separate dying ember wrought its ghost upon the floor.
     Eagerly I wished the morrow;—vainly I had sought to borrow
     From my books surcease of sorrow—sorrow for the lost Lenore—
@@ -58,31 +53,30 @@ Some late visitor entreating entrance at my chamber door;—
 “Sir,” said I, “or Madam, truly your forgiveness I implore;
     But the fact is I was napping, and so gently you came rapping,
     And so faintly you came tapping, tapping at my chamber door,
-That I scarce was sure I heard you”—here I opened wide the door;—
+        That I scarce was sure I heard you”—here I opened wide the door;—
             Darkness there and nothing more.
 
     Deep into that darkness peering, long I stood there wondering, fearing,
-Doubting, dreaming dreams no mortal ever dared to dream before;
+    Doubting, dreaming dreams no mortal ever dared to dream before;
     But the silence was unbroken, and the stillness gave no token,
-    And the only word there spoken was the whispered word, “Lenore?”
+    And the only word there spoken was the whispered word, “Lenore ?”
 This I whispered, and an echo murmured back the word, “Lenore!”—
             Merely this and nothing more.
 
     Back into the chamber turning, all my soul within me burning,
-Soon again I heard a tapping somewhat louder than before.
+    Soon again I heard a tapping somewhat louder than before.
     “Surely,” said I, “surely that is something at my window lattice;
       Let me see, then, what thereat is, and this mystery explore—
 Let my heart be still a moment and this mystery explore;—
             ’Tis the wind and nothing more!”
 
     Open here I flung the shutter, when, with many a flirt and flutter,
-In there stepped a stately Raven of the saintly days of yore;
+    In there stepped a stately Raven of the saintly days of yore;
     Not the least obeisance made he; not a minute stopped or stayed he;
-    But, with mien of lord or lady, perched above my chamber door—
+But, with mien of lord or lady, perched above my chamber door—
 Perched upon a bust of Pallas just above my chamber door—
-            Perched, and sat, and nothing more.
-</p>
-</div>
+Perched, and sat, and nothing more.</p>
+      </div>
       <label  >Email</label>
       <input  className=" login-form input" type="email" onChange={(e) => setEmail(e.target.value)} />
       <label  >Password</label>
